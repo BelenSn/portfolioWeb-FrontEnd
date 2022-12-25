6d753edd60d1c4524831cde6e804cf66d6b8cf69
Belen <code.belen@gmail.com>
2022-12-25 12:25:07 -0300

    Se realizo la validacion de campos del login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  formLogin: FormGroup;
+
+  constructor(private formBuilder:FormBuilder) { 
+    this.formLogin = this.formBuilder.group(
+      {
+        email:['',[Validators.required,Validators.email]],
+        password: ['',[Validators.required,Validators.minLength(8)]],
+      }
+    )
+  }
 
   ngOnInit(): void {
+  }
+
+  get Email(){
+    return this.formLogin.get('email');
+  }
+
+  get Password(){
+    return this.formLogin.get('password');
   }
 
 }
