cee084003c51d97464dd79dac9853537d53a38ca
Belen <code.belen@gmail.com>
2023-01-02 17:22:10 -0300

    Se colocaron alertas en los modales | Se pusieron condiciones con ngIf para mostrar u ocultar componentes segun si existieran elementos

--- src/app/models/project.ts
+++ src/app/models/project.ts
@@ -1,6 +1,7 @@
 export interface Project{
     idProject: number;
     tituloProject: string;
+    fechaProject: string;
     descripcionProject: string;
     imagenProject: string;
     linkProject: string;
